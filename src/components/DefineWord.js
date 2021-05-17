679ed2a53c1460e2d8c380bcfba1a25b08fc99a0
JorPan <jbpanasewicz@gmail.com>
2021-05-17 11:20:37 -0600

    refactored define word to functional, hid api key

--- src/components/DefineWord.js
+++ src/components/DefineWord.js
@@ -1,16 +1,16 @@
-import React, { Component } from "react";
-const apiKey = "858828029fmsh37b23938027c823p15044ejsn9d489660ef51";
+import React, { useState, useEffect } from "react";
+import { defineWordKey } from "../config";
+const apiKey = defineWordKey;
+const definitionURL = "https://wordsapiv1.p.rapidapi.com/words";
 
-export default class DefineWord extends Component {
-  state = {
-    word: "",
-    definition: "undefined",
-  };
+export default function DefineWord() {
+  const [word, setWord] = useState("");
+  const [definition, setDefinition] = useState("undefined");
 
-  search = (event) => {
+  const search = (event) => {
     event.preventDefault();
-    let wordToSearch = this.state.word;
-    fetch(`https://wordsapiv1.p.rapidapi.com/words/${wordToSearch}`, {
+    let wordToSearch = word;
+    fetch(`${definitionURL}/${wordToSearch}`, {
       method: "GET",
       headers: {
         "x-rapidapi-key": apiKey,
@@ -20,42 +20,40 @@ export default class DefineWord extends Component {
       .then((response) => response.json())
       .then((wordData) => {
         if (wordData.success === false) {
-          this.setState({ definition: "Not Found" });
+          setDefinition("Not Found");
         } else if (wordData.results) {
-          this.setState({ definition: wordData.results[0].definition });
+          setDefinition(wordData.results[0].definition);
         } else {
-          this.setState({ definition: "Not Found" });
+          setDefinition("Not Found");
         }
       });
   };
 
-  handleChange = (event) => {
-    this.setState({ word: event.target.value });
+  const handleChange = (event) => {
+    setWord(event.target.value);
   };
 
-  hideDefinition = () => {
-    this.setState({ definition: "undefined" });
+  const hideDefinition = () => {
+    setDefinition("undefined");
   };
 
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.search}>
-          <input
-            onChange={this.handleChange}
-            value={this.state.word}
-            name="input"
-            type="text"
-            placeholder="Word Lookup"
-          ></input>
-          <input type="submit"></input>
-        </form>
-        {this.state.definition === "undefined" ? null : (
-          <p onClick={this.hideDefinition} className="definition">
-            {this.state.definition}
-          </p>
-        )}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <form onSubmit={search}>
+        <input
+          onChange={handleChange}
+          value={word}
+          name="input"
+          type="text"
+          placeholder="Word Lookup"
+        ></input>
+        <input type="submit"></input>
+      </form>
+      {definition === "undefined" ? null : (
+        <p onClick={hideDefinition} className="definition">
+          {definition}
+        </p>
+      )}
+    </div>
+  );
 }

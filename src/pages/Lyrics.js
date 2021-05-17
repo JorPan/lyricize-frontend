f8f1cb81e6983f892a90aab271ae2e2b8d049263
JorPan <jbpanasewicz@gmail.com>
2021-05-17 11:22:13 -0600

    refactor lyrics page to functional component

--- src/pages/Lyrics.js
+++ src/pages/Lyrics.js
@@ -2,15 +2,13 @@ import React from "react";
 import Favorites from "../components/Favorites";
 import MySongs from "../components/MySongs";
 
-export default class Lyrics extends React.Component {
-  render() {
-    return (
-      <div className="my-songs-page">
-        <div className="songsection">
-          <Favorites />
-          <MySongs />
-        </div>
+export default function Lyrics() {
+  return (
+    <div className="my-songs-page">
+      <div className="songsection">
+        <Favorites />
+        <MySongs />
       </div>
-    );
-  }
+    </div>
+  );
 }
